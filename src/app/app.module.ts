9854997771a845579a428449446ecefedb8b8f44
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-07 10:32:28 -0500

    Created responsive navbar with teal theme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RebudgetHomepageComponent } from './pages/rebudget-homepage/rebudget-homepage.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RebudgetHomepageComponent
+    RebudgetHomepageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
