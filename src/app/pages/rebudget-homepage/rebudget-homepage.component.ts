da2bfec6171a3c5d9e0ec62746a56f6a5f34534b
Jorge Morin <jmorin1@hawk.iit.edu>
2023-06-14 15:44:21 -0500

    Users can now update their expenses

--- src/app/pages/rebudget-homepage/rebudget-homepage.component.ts
+++ src/app/pages/rebudget-homepage/rebudget-homepage.component.ts
@@ -19,6 +19,7 @@ export class RebudgetHomepageComponent {
   annualWage = 0;
   totalProjectSavings = 0;
   ageDifference = 0;
+  InputValue = 1;
 
   expenseTableMap: Map<string, number> = new Map<string, number>([
     ['Housing', 1600],
@@ -63,6 +64,18 @@ export class RebudgetHomepageComponent {
     return Array.from(this.expenseTableMap.entries())
   }
 
+  updateExpenseTableMap(expenseItem: string, expenseCost: Event) {
+    let inputValue = (expenseCost.target as HTMLInputElement).value;
+    if(inputValue == ''){
+      inputValue = '0';
+    }
+    let numericValue = inputValue.replace(/[^0-9.]/g, '');
+    let truncatedValue = Math.floor(parseFloat(numericValue) * 100) / 100;
+
+    this.expenseTableMap.set(expenseItem, truncatedValue);
+    this.calculateProjectedExpenses();
+  }
+
   async calculateProjectedSavings() {
     if (this.budgetForm.valid && this.userState != this.INITIAL_USER_STATE_MESSAGE) {
       let projectedGrossPay = 0;
@@ -87,12 +100,9 @@ export class RebudgetHomepageComponent {
       this.calculateMedicare(annualGrossPay);
       await this.calculateStateTax(annualGrossPay);
       this.calculateProjectedIncome();
+      this.calculateProjectedExpenses();
 
-      let totalProjectedExpenses = this.expenseTableMap.get('Total Annual Projected Expenses') ?? 0;
-      totalProjectedExpenses = totalProjectedExpenses * this.ageDifference;
-      this.expenseTableMap.set('Total Projected Expenses', totalProjectedExpenses);
-
-      this.totalProjectSavings = (this.incomeTableMap.get('Total Projected Income') ?? 0) - (totalProjectedExpenses);
+      this.totalProjectSavings = (this.incomeTableMap.get('Total Projected Income') ?? 0) - (this.expenseTableMap.get('Total Projected Expenses') ?? 0);
 
     }
   }
@@ -100,7 +110,7 @@ export class RebudgetHomepageComponent {
   async calculateFederalTax(annualGrossPay: number) {
     let projectedFederalTax = 0;
     let taxableIncome = annualGrossPay - this.US_STANDARD_DEDUCTION;
-  
+
     if (taxableIncome <= 0) {
       this.incomeTableMap.set('Projected Federal Witholding', projectedFederalTax);
     } else {
@@ -123,7 +133,7 @@ export class RebudgetHomepageComponent {
   async calculateStateTax(annualGrossPay: number) {
     let projectedStateTax = 0;
     let taxableIncome = annualGrossPay - this.US_STANDARD_DEDUCTION;
-  
+
     if (taxableIncome <= 0) {
       this.incomeTableMap.set('Projected State Witholding', projectedStateTax);
     } else {
@@ -133,9 +143,20 @@ export class RebudgetHomepageComponent {
     }
   }
 
-  calculateProjectedIncome(){
+  calculateProjectedIncome() {
     let projectedIncome = (this.incomeTableMap.get('Projected Gross Pay') ?? 0) - (this.incomeTableMap.get('Projected Federal Witholding') ?? 0) - (this.incomeTableMap.get('Projected Social Security') ?? 0) - (this.incomeTableMap.get('Projected Medicare') ?? 0) - (this.incomeTableMap.get('Projected State Witholding') ?? 0);
     this.incomeTableMap.set('Total Projected Income', projectedIncome);
+  }
+
+  calculateProjectedExpenses() {
+    const totalMonthlyExpenses = (this.expenseTableMap.get('Housing') ?? 0) + (this.expenseTableMap.get('Transportation') ?? 0) + (this.expenseTableMap.get('Food') ?? 0) + (this.expenseTableMap.get('Healthcare') ?? 0) + (this.expenseTableMap.get('Entertainment') ?? 0) + (this.expenseTableMap.get('Apparel') ?? 0) + (this.expenseTableMap.get('Misc') ?? 0);
+    this.expenseTableMap.set('Total Monthly Projected Expenses', totalMonthlyExpenses);
+    let monthlyProjectedExpenses = this.expenseTableMap.get('Total Monthly Projected Expenses') ?? 0;
+    let annualProjectedExpenses = monthlyProjectedExpenses * 12;
+    this.expenseTableMap.set('Total Annual Projected Expenses', annualProjectedExpenses);
+    let totalProjectedExpenses = this.expenseTableMap.get('Total Annual Projected Expenses') ?? 0;
+    totalProjectedExpenses = totalProjectedExpenses * this.ageDifference;
+    this.expenseTableMap.set('Total Projected Expenses', totalProjectedExpenses);
   }
 
 
@@ -148,13 +169,7 @@ export class RebudgetHomepageComponent {
 
   ngOnInit() {
     this.LIST_ALL_US_STATES = new US_States().US_States;
-    const sum = Array.from(this.expenseTableMap.values()).reduce((acc, curr) => acc + curr, 0);
-    this.expenseTableMap.set('Total Monthly Projected Expenses', sum);
-    let monthlyProjectedExpenses = this.expenseTableMap.get('Total Monthly Projected Expenses') ?? 0;
-    let annualProjectedExpenses = monthlyProjectedExpenses * 12;
-    this.expenseTableMap.set('Total Annual Projected Expenses', annualProjectedExpenses);
-
-
+    this.calculateProjectedExpenses();
   }
 
 
